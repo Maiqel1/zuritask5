75c8176aa7be9f55909dffb0b4f89ee09186dc47
Maiqel1 <66220194+Maiqel1@users.noreply.github.com>
2021-05-20 16:17:58 +0100

    Add files via upload

---
+++ zuritask.js
@@ -0,0 +1,66 @@
+   
+
+const yugioh = (n) =>{
+    let result = [];
+
+    if (isNaN(n) || parseInt(n) < 1){
+        console.log(`invalid parameter: ${n}`);
+        return `invalid parameter: ${n}`;
+    } 
+
+    for (let i = 1; i <=n; i++) {
+        let localvar = [];
+        let fvar = "";  
+
+        if (i % 2 == 0) {
+            localvar.push("yu")
+        }
+        if (i % 3 == 0) {
+            localvar.push("gi")
+        }
+        if (i % 5 == 0) {
+            localvar.push("oh")
+        }
+    
+
+       fvar = localvar.join("-");
+        
+       if (fvar) {
+           result.push(fvar)
+       } else {
+           result.push(i)
+       }
+         
+     }
+         console.log(result);
+         return result;
+   }
+
+   yugioh(100)  
+
+
+    // convert farh to Celcius// 
+
+    function convertFahrToCelcius(f){
+    
+        let c = (f - 32) * 5/9;
+        let fahr = Object.prototype.toString.call(f);
+    
+        if (fahr ==="[object Number]" || (fahr ==="[object String]" && !isNaN(f))) {
+            return c;
+        } else {
+            let param  = fahr.split(" ");
+            param[1] = param[1].substring(0, param[1]. length-1);
+    
+            return `${JSON.stringify(f)} is not a valid number but a/an ${param[1]}`
+        }
+        
+    }
+    
+    console.log(convertFahrToCelcius());
+    
+    
+    
+    
+    
+    
